17ee0d4709a751df6849a99f4edf24a3e68fc85f
Ilya Shevtsov <shevtsovis@novotelecom.ru>
2025-10-19 13:57:34 +0800

    added repo

---
+++ composeApp/src/commonMain/kotlin/com/example/partnerkin/data/ConferenceRepositoryImpl.kt
@@ -0,0 +1,6 @@
+package com.example.partnerkin.data
+
+import com.example.partnerkin.domain.ConferenceRepository
+
+class ConferenceRepositoryImpl: ConferenceRepository {
+}
---
+++ composeApp/src/commonMain/kotlin/com/example/partnerkin/domain/ConferenceRepository.kt
@@ -0,0 +1,4 @@
+package com.example.partnerkin.domain
+
+interface ConferenceRepository {
+}
